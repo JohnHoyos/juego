6e7a187606c542d9598b1ca7523edb9c038052ba
John Hoyos <jbayron11222@gmail.com>
2023-01-13 16:10:22 -0500

    Segundo commit arreglo de detalles

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador(){
+    alert("Me presionaste")
+}
+let botonMascotaJugador= document.getElementById("boton-mascota")
+
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
