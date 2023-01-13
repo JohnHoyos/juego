acc1194fed44a453d8fb2e2454b2ca55cc91e988
John Hoyos <jbayron11222@gmail.com>
2023-01-13 17:18:09 -0500

    Optimizacion 1 JS

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,21 @@
-function seleccionarMascotaJugador(){
-    alert("Me presionaste")
-}
-let botonMascotaJugador= document.getElementById("boton-mascota")
+function iniciarJuego(){
+let botonMascotaJugador = document.getElementById("boton-mascota")
 
 botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+}
+function encontrarSeleccion(mascotas,tamano){
+    for(i=0;i<tamano;i++){
+        if(mascotas[i].checked)
+            return mascotas[i].id
+    }
+ }
+
+function seleccionarMascotaJugador(){
+    let listadoMascotas = document.getElementsByName("mascota")
+    let tamano = listadoMascotas.length
+    let seleccion = encontrarSeleccion(listadoMascotas,tamano)
+        
+    alert("Elegiste: " + seleccion +" como mascota")
+}
+
+window.addEventListener('load',iniciarJuego)
