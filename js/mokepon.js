0081c4f5904e37d3d1520c8f75cdbc6b474d3f93
John Hoyos <jbayron11222@gmail.com>
2023-01-14 09:16:38 -0500

    Optimizacion 2 JS

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,21 +1,25 @@
 function iniciarJuego(){
+//Aca se toma la varible que contiene todo el boton
 let botonMascotaJugador = document.getElementById("boton-mascota")
-
+//Aca se le asigna un listener para cuando le den click
 botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
 }
-function encontrarSeleccion(mascotas,tamano){
-    for(i=0;i<tamano;i++){
-        if(mascotas[i].checked)
-            return mascotas[i].id
+function encontrarSeleccion(listado,opcion){ //funcion generica para grupos4
+    var aleatorio = Math.floor(Math.random()*(listado.length))
+    if(opcion) return listado[aleatorio].id 
+    else{
+        for(i=0;i<listado.length;i++){
+            if(listado[i].checked)
+                return listado[i].id
+        }
     }
  }
-
-function seleccionarMascotaJugador(){
+function seleccionarMascotaJugador(){//muestra en pantalla la seleccion
     let listadoMascotas = document.getElementsByName("mascota")
-    let tamano = listadoMascotas.length
-    let seleccion = encontrarSeleccion(listadoMascotas,tamano)
-        
-    alert("Elegiste: " + seleccion +" como mascota")
+    let mascotaJugador = document.getElementById("mascota-jugador")
+    let mascotaEnemigo = document.getElementById("mascota-enemigo")
+    mascotaJugador.innerHTML= encontrarSeleccion(listadoMascotas)
+    mascotaEnemigo.innerHTML= encontrarSeleccion(listadoMascotas,true)
+    //alert("Elejiste " + seleccion + " como mascota")
 }
-
 window.addEventListener('load',iniciarJuego)
