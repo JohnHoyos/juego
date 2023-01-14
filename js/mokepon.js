a1f8206d9e7880a3a1794a69c250c97c0fa01c54
John Hoyos <jbayron11222@gmail.com>
2023-01-14 17:22:49 -0500

    Prueba antes de reducir codigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,5 +1,8 @@
 let ataqueJugador = ""
 let ataqueEnemigo = ""
+let vidaJugador = 3
+let vidaEnemigo = 3
+
 window.addEventListener('load',iniciarJuego)
 function iniciarJuego(){
 let botonMascotaJugador = document.getElementById("boton-mascota")//Aca se toma la varible que contiene todo el boton
@@ -7,13 +10,10 @@ botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)//Aca se 
 }
 function encontrarSeleccion(listado,opcion){ //funcion generica para grupos4
     var aleatorio = Math.floor(Math.random()*(listado.length))
-    if(opcion) return listado[aleatorio].id 
-    else{
-        for(i=0;i<listado.length;i++){
-            if(listado[i].checked)
-                return listado[i].id
-        }
-    }
+    if(opcion) 
+        return listado[aleatorio].id 
+    else
+        for(i=0;i<listado.length;i++){ if(listado[i].checked);return listado[i].id}
  }
 function seleccionarMascotaJugador(){//muestra en pantalla la seleccion
     let listadoMascotas = document.getElementsByName("mascota")
@@ -37,7 +37,37 @@ function ataqueAgua(){ataqueJugador = "AGUA";mostrarAtaques()}
 function ataqueTierra(){ataqueJugador = "TIERRA";mostrarAtaques()}
 function mostrarAtaques(){
     let listaAtaques = document.getElementsByName("ataques")
-    ataqueEnemigo = encontrarSeleccion(listaAtaques,true)
-    document.getElementById("ataque-jugador").innerHTML = ataqueJugador
-    document.getElementById("ataque-enemigo").innerHTML = ataqueEnemigo
+    ataqueEnemigo = encontrarSeleccion(listaAtaques,true).toUpperCase()
+    let mensaje = document.createElement("p")
+    let seccion_mensajes = document.getElementById("mensajes")
+    mensaje.innerHTML = "Tu mascota atacó con " + ataqueJugador + " las mascota del enemigo atacó con " + ataqueEnemigo + " -  " + ganador() + " 🎉🎉"
+    seccion_mensajes.appendChild(mensaje)
+    actualizarVidas(ganador())
+}
+function ganador(){
+    let resultado = ""
+    if(ataqueJugador == ataqueEnemigo)
+     resultado = "EMPATE"
+    else if(ataqueJugador == "FUEGO" && ataqueEnemigo == "TIERRA")
+     resultado = "GANASTE"
+    else if(ataqueJugador == "AGUA" && ataqueEnemigo == "FUEGO")
+     resultado = "GANASTE"
+    else if(ataqueJugador == "TIERRA" && ataqueEnemigo == "AGUA")
+     resultado = "GANASTE"
+    else resultado = "PERDISTE"
+
+    return resultado
+}
+
+function actualizarVidas(resul){
+    let msgJugador = document.getElementById("vida-jugador")
+    let msgEnemigo = document.getElementById("vida-enemigo")
+    if(vidaJugador == 0)
+    alert(document.getElementById("mascota-jugador").innerText + " PERDIÓ")
+    else if(vidaEnemigo == 0) alert(document.getElementById("mascota-enemigo").innerText + " PERDIÓ")
+    else if(resul == "PERDISTE")vidaJugador--
+    else if(resul == "GANASTE")vidaEnemigo--
+
+    msgJugador.innerHTML = vidaJugador
+    msgEnemigo.innerHTML = vidaEnemigo
 }
