4abaf9ff87fe68d0a96ba25c7657fd00401b5f7f
John Hoyos <jbayron11222@gmail.com>
2023-01-14 12:51:55 -0500

    Optimizacion 3 JS y anexo de carpeta CSS

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,25 +1,51 @@
+let ataqueJugador = ""
+let ataqueEnemigo = ""
+window.addEventListener('load',iniciarJuego)
 function iniciarJuego(){
-//Aca se toma la varible que contiene todo el boton
-let botonMascotaJugador = document.getElementById("boton-mascota")
-//Aca se le asigna un listener para cuando le den click
-botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+let botonMascotaJugador = document.getElementById("boton-mascota")//Aca se toma la varible que contiene todo el boton
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)//Aca se le asigna un listener para cuando le den click
 }
 function encontrarSeleccion(listado,opcion){ //funcion generica para grupos4
     var aleatorio = Math.floor(Math.random()*(listado.length))
     if(opcion) return listado[aleatorio].id 
     else{
-        for(i=0;i<listado.length;i++){
-            if(listado[i].checked)
-                return listado[i].id
-        }
+        listado.forEach(element => {
+            if(element.checked) return element
+        });
     }
  }
 function seleccionarMascotaJugador(){//muestra en pantalla la seleccion
     let listadoMascotas = document.getElementsByName("mascota")
+    
     let mascotaJugador = document.getElementById("mascota-jugador")
     let mascotaEnemigo = document.getElementById("mascota-enemigo")
+    
+    let botonFuego = document.getElementById("fuego")
+    let botonAgua = document.getElementById("agua")
+    let botonTierra = document.getElementById("tierra")
+    
     mascotaJugador.innerHTML= encontrarSeleccion(listadoMascotas)
     mascotaEnemigo.innerHTML= encontrarSeleccion(listadoMascotas,true)
-    //alert("Elejiste " + seleccion + " como mascota")
+  
+    botonFuego.addEventListener('click',ataqueFuego)
+    botonAgua.addEventListener('click',ataqueAgua)
+    botonTierra.addEventListener('click',ataqueTierra)
 }
-window.addEventListener('load',iniciarJuego)
+function ataqueFuego(){
+    ataqueJugador = "FUEGO"
+    mostrarAtaques()
+}
+function ataqueAgua(){
+    ataqueJugador = "AGUA"
+    mostrarAtaques()
+}
+function ataqueTierra(){
+    ataqueJugador = "TIERRA"
+    mostrarAtaques()
+}
+function mostrarAtaques(){
+    let listaAtaques = document.getElementsByName("ataques")
+    ataqueEnemigo = encontrarSeleccion(listaAtaques,true)
+    document.getElementById("ataque-jugador").innerHTML = ataqueJugador
+    document.getElementById("ataque-enemigo").innerHTML = ataqueEnemigo
+}
