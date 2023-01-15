19d0f90411b505307296346af583fefdf0d0ca6f
John Hoyos <jbayron11222@gmail.com>
2023-01-15 15:00:18 -0500

    Funcional es posible mejorarlo mucho mas

--- js/mokepon.js
+++ js/mokepon.js
@@ -2,11 +2,28 @@ let ataqueJugador = ""
 let ataqueEnemigo = ""
 let vidaJugador = 3
 let vidaEnemigo = 3
-window.addEventListener('load',iniciarJuego)
-function iniciarJuego(){
-let botonSeleccionar = document.getElementById("boton-seleccionar")//Aca se toma la varible que contiene todo el boton
-botonSeleccionar.addEventListener('click',actualizarMascotas)//Aca se le asigna un listener para cuando le den click
-}
+let variable = "a prueba"
+let variable2 = "de todo"
+window.addEventListener('load',() =>{
+
+    let botonSeleccionar = document.getElementById("boton-seleccionar")//Aca se toma la varible que contiene todo el boton
+    botonSeleccionar.addEventListener('click',() => {
+        let listadoMascotas = document.getElementsByName("mascotas")
+        let mascotaJugador = document.getElementById("mascota-jugador")
+        let mascotaEnemigo = document.getElementById("mascota-enemigo")
+        let botonFuego = document.getElementById("fuego")
+        let botonAgua = document.getElementById("agua")
+        let botonTierra = document.getElementById("tierra")
+        let mascotaElegidaJugador = encontrarSeleccion(listadoMascotas) 
+        let mascotaElegidaEnemigo = encontrarSeleccion(listadoMascotas,true)
+        mascotaJugador.innerHTML = mascotaElegidaJugador
+        mascotaEnemigo.innerHTML = mascotaElegidaEnemigo
+        actualizarVidas()
+        botonFuego.addEventListener('click',ataqueFuego)
+        botonAgua.addEventListener('click',ataqueAgua)
+        botonTierra.addEventListener('click',ataqueTierra)  
+})})
+
 function encontrarSeleccion(listado,opcion){ //funcion generica para grupos4
     var aleatorio = Math.floor(Math.random()*(listado.length))
     if(opcion){return listado[aleatorio].id }
@@ -17,21 +34,6 @@ function encontrarSeleccion(listado,opcion){ //funcion generica para grupos4
             }
         }
 }
-function actualizarMascotas(){//muestra en pantalla la seleccion
-    let listadoMascotas = document.getElementsByName("mascotas")
-    let mascotaJugador = document.getElementById("mascota-jugador")
-    let mascotaEnemigo = document.getElementById("mascota-enemigo")
-    let botonFuego = document.getElementById("fuego")
-    let botonAgua = document.getElementById("agua")
-    let botonTierra = document.getElementById("tierra")
-    
-    mascotaJugador.innerHTML= encontrarSeleccion(listadoMascotas)  
-    mascotaEnemigo.innerHTML= encontrarSeleccion(listadoMascotas,true)
-    actualizarVidas()
-    botonFuego.addEventListener('click',ataqueFuego)
-    botonAgua.addEventListener('click',ataqueAgua)
-    botonTierra.addEventListener('click',ataqueTierra)
-}
 function ataqueFuego(){ataqueJugador = "FUEGO";actualizarAtaques()}
 function ataqueAgua(){ataqueJugador = "AGUA";actualizarAtaques()}
 function ataqueTierra(){ataqueJugador = "TIERRA";actualizarAtaques()}
@@ -40,9 +42,15 @@ function actualizarAtaques(){
     ataqueEnemigo = encontrarSeleccion(listaAtaques,true).toUpperCase()
     let mensaje = document.createElement("p")
     let seccion_mensajes = document.getElementById("mensajes")
+    if(vidaEnemigo == 0){
+        mensaje.innerHTML = "Enemigo ha perdido"   
+    }else if(vidaJugador == 0)mensaje.innerHTML = "Jugador ha perdido"
+    else{
     mensaje.innerHTML = "Tu mascota atacó con " + ataqueJugador + " la mascota del enemigo atacó con " + ataqueEnemigo + " -  " + ganador()
-    seccion_mensajes.appendChild(mensaje)
     actualizarVidas(ganador())
+    }
+    seccion_mensajes.appendChild(mensaje)
+
 }
 function ganador(){
     let resultado = ""
@@ -60,11 +68,9 @@ function ganador(){
 function actualizarVidas(resul){
     let msgJugador = document.getElementById("vida-jugador")
     let msgEnemigo = document.getElementById("vida-enemigo")
-    if(vidaJugador == 0)
-    alert(document.getElementById("mascota-jugador").innerText + " PERDIÓ")
-    else if(vidaEnemigo == 0) alert(document.getElementById("mascota-enemigo").innerText + " PERDIÓ")
-    else if(resul == "PERDISTE 😢")vidaJugador--
+    if(resul == "PERDISTE 😢")vidaJugador--
     else if(resul == "GANASTE 🎉🎉")vidaEnemigo--
+
     msgJugador.innerHTML = vidaJugador
     msgEnemigo.innerHTML = vidaEnemigo
     
