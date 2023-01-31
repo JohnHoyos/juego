4350a96555063cce7d48a8185ec696b102919a9e
John Hoyos <jbayron11222@gmail.com>
2023-01-30 20:28:15 -0500

    prueba sincronizacion 4

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ app.get("/unirse", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin","*")
     res.send(id)
 });
+
+
+
 app.post("/mokepon/:jugadorId", (req, res) =>{
     const jugadorId = req.params.jugadorId || ""
     const nombre = req.body.mokepon || ""
