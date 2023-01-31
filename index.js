2062a66df4f584fcd21f1c84dad1594398dfb697
John Hoyos <jbayron1122@gmail.com>
2023-01-30 19:37:13 -0600

    Juego consumiendo de servidor

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ class Mokepon{
         this.nombre = nombre
     }
 }
-
 app.get("/unirse", (req, res) => {
     const id = `${Math.random()}`
     const jugador =  new Jugador(id)
@@ -29,7 +28,6 @@ app.get("/unirse", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin","*")
     res.send(id)
 });
-
 app.post("/mokepon/:jugadorId", (req, res) =>{
     const jugadorId = req.params.jugadorId || ""
     const nombre = req.body.mokepon || ""
