63614fb95c03fca402bdb244f2f9bc8ead10dc5e
John Hoyos <jbayron1122@gmail.com>
2023-01-30 19:38:33 -0600

    Sincronizacoin 7

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get("/unirse", (req, res) => {
     res.send(id)
 });
 app.post("/mokepon/:jugadorId", (req, res) =>{
-    
     const jugadorId = req.params.jugadorId || ""
     const nombre = req.body.mokepon || ""
     const mokepon = new Mokepon(nombre)
